922c1ca5882ded921c60bbafe7c3605e64b8ed15
Дмитрий Попков <dmitriypopkov98@gmail.com>
2022-07-18 23:43:36 +0300

    mongo test

--- server/index.js
+++ server/index.js
@@ -1,10 +1,27 @@
-// Section 1
 const express = require('express');
-const axios = require('axios');
 const path = require('path');
+const MongoClient = require("mongodb").MongoClient;
 
 const app = express();
 app.use(express.static(path.join(__dirname, '..', 'public')));
+
+var url = "mongodb://localhost:27017";
+const mongoClient = new MongoClient(url);
+
+async function run() {
+    try {
+        await mongoClient.connect();
+        const db = mongoClient.db("test");
+        const collection = db.collection("testtest");
+        const results = await collection.find().toArray();
+        console.log(results);
+    }catch(err) {
+        console.log(err);
+    } finally {
+        await mongoClient.close();
+    }
+}
+run();
 
 app.listen(3000, () => {
     console.log('server started on port 3000');

d0c5446e87fbed8f2aea97e5085cd15a9cd0a2c4
Дмитрий Попков <dmitriypopkov98@gmail.com>
2022-07-19 21:39:31 +0300

    configure ts

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
-const express = require('express');
-const path = require('path');
-const MongoClient = require("mongodb").MongoClient;
+import express from "express";
+import { MongoClient } from "mongodb";
+import path from "path";
 
 const app = express();
 app.use(express.static(path.join(__dirname, '..', 'public')));

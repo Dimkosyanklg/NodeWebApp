59a31ca826355232db508e1feffd846195859a84
Дмитрий Попков <dmitriypopkov98@gmail.com>
2022-07-20 20:43:58 +0300

    i beat ts imports finally

--- server/index.ts
+++ server/index.ts
@@ -1,28 +1,17 @@
 import express from "express";
 import { MongoClient } from "mongodb";
 import path from "path";
+import { userRouter } from "./routes/userRouter";
+
 
 const app = express();
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 var url = "mongodb://localhost:27017";
-const mongoClient = new MongoClient(url);
+export const mongoClient = new MongoClient(url);
 
-async function run() {
-    try {
-        await mongoClient.connect();
-        const db = mongoClient.db("test");
-        const collection = db.collection("testtest");
-        const results = await collection.find().toArray();
-        console.log(results);
-    }catch(err) {
-        console.log(err);
-    } finally {
-        await mongoClient.close();
-    }
-}
-run();
+app.use("/users", userRouter);
 
-app.listen(3000, () => {
+app.listen(3001, () => {
     console.log('server started on port 3000');
 });
--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-export const App: React.FC<Props> = ({}) => {
-    return <div>тест</div>
-}
